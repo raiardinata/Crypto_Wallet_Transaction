2183647e721bb981e04a5fcb9628545500743d48
IemmaJedi <59637649+raiardinata@users.noreply.github.com>
2023-09-15 09:30:08 +0700

    Update dbConfig.ts

--- src/dbConfig.ts
+++ src/dbConfig.ts
@@ -6,10 +6,10 @@ dotenv.config({ path: '/home/raiardinata/ra_Server/typescript/playground/SW_Test
 export default {
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'sw_test',
+    host: process.env.DB_HOST || '',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || '',
   },
   migrations: {
     tableName: 'knex_migrations',
